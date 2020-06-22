6907f7c86bde2d02cc16240fb09bae85634c4ddc
Hector Hernandez <hectorohernz@gmail.com>
2020-06-22 16:09:39 -0400

    added home-page styles and content WIP

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import './Home.css';
 
 
 export default function Home() {
+    const [isLogin, setIsLogin] = useState(false);
+    const [isSigningUp, setIsSigningUp] = useState(false);
+    
     return (
-        <section className="home">
-           
+        <section className={isLogin ? "login" : isSigningUp === true ? "sign-up" : "home" }>
+           <div className="main__logo">
+                <img src="/images/logo.png"/>
+           </div>
+           <div className="whitebox">
+            { isLogin === true ?(
+                <h1>Login</h1>
+            ): isSigningUp === true ? (
+                <h1>Sign Up </h1>
+            ): (
+                <div className="whitebox-btn">
+                    <button onClick={ () => setIsLogin(true)}>Login</button>
+                     <button onClick={ () => setIsSigningUp(true)} className="whitebox__signup">Sign Up</button>
+                </div>
+            )}
+           </div>
+
+
         </section>
     )
 }
