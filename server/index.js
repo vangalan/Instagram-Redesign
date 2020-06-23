46a8d52a2ce06ca8d4da2405fb551c35df71debb
Hector Hernandez <hectorohernz@gmail.com>
2020-06-23 12:49:36 -0400

    adding styles

--- server/index.js
+++ server/index.js
@@ -1,2 +1,8 @@
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+
+app.listen( PORT, () => {
+    console.log(`PORT Is Listening on localhost:${PORT}`)
+})
