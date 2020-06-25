20d71c7e13e4d7c35d521a6c231058f6b4d8082f
alan vang <vangnalan@gmail.com>
2020-06-25 13:16:55 -0400

    updated nav bar

---
+++ src/components/main/Main.js
@@ -0,0 +1,11 @@
+import React, { Component, useState } from "react";
+import './Home.css';
+
+export default function Main() {
+    return (
+        <div>
+            <h1>Main</h1>
+        </div>
+    )
+};
+
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { FaHome } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+import { FaPaperPlane } from 'react-icons/fa';
 import './Nav.css'
 
 export default function NavBar() {
@@ -7,28 +11,33 @@ export default function NavBar() {
     const [clicked, hasBeenClickedOn] = useState(false)
 
     return (
-            <div className="container">
-                <header>
-                    <nav className={`navbar ${clicked ? "change" : ""}`}>
-                        <div className={`hamburger--container`} onClick={() => {
-                            if (clicked) {
-                                hasBeenClickedOn(false);
-                            } else {
-                                hasBeenClickedOn(true);
-                            }
-                        }}>
-                            <div className="hamburger--line line--1"></div>
-                            <div className="hamburger--line line--2"></div>
-                            <div className="hamburger--line line--3"></div>
-                        </div>
-                        <ul className="ul--navlinks">
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/profile">Profile</a></li>
-                            <li><a href="/feed">Feed</a></li>
-                            <li><a href="/search">Search</a></li>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
+        <div className="container">
+            <header>
+                <nav className={`navbar ${clicked ? "change" : ""}`}>
+                    <div className={`hamburger--container`} onClick={() => {
+                        if (clicked) {
+                            hasBeenClickedOn(false);
+                        } else {
+                            hasBeenClickedOn(true);
+                        }
+                    }}>
+                        <div className="hamburger--line line--1"></div>
+                        <div className="hamburger--line line--2"></div>
+                        <div className="hamburger--line line--3"></div>
+                    </div>
+                    <ul className="ul--navlinks">
+                        <img src="https://i.ya-webdesign.com/images/instagram-circle-png-7.png" alt="Instagram picture" width="100" height="100"></img>
+                        <h1>Alan Vang</h1>
+                        <h3>500 followers</h3>
+
+                        <li><a href="/profile"><h3><FaUser />  Profile</h3></a></li>
+                        <li><a href="/feed"><h3><FaHome />   Feed</h3></a></li>
+                        <li><a href="/search"><h3><FaSearch />   Search</h3></a></li>
+                        <li><a href="/"><h3><FaPaperPlane />   Log off</h3></a></li>
+
+                    </ul>
+                </nav>
+            </header>
+        </div>
     );
 }
