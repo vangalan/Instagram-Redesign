5136dbadac86de8eba21f4ae7ce2544cb8f6178d
Hector Hernandez <hectorohernz@gmail.com>
2020-06-30 08:52:30 -0400

    connected back-end with sign up/sign in

---
+++ src/components/alerts/SuccesReg.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './alert.css';
+
+export default class SuccessfulLogin extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+
+         }
+    }
+    render() {
+        return (
+            <section className="Profile-Custom-Login">
+                
+            </section>
+        )
+    }
+}
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,22 +1,67 @@
-import React from  "react";
 import './login.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
+import { Redirect } from 'react-router-dom';
+import Alert from '../Test/layout/Alert';
 
-export default function Login() {
-    return (
-        <div className="">
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+                password:"",
+                username:""
+        };
+        this.changeHandler = this.changeHandler.bind(this);
+        this.submitHandler =  this.submitHandler.bind(this);
+    }
+    changeHandler(e){
+        this.setState({   [e.target.name]: e.target.value    });
+    }
+
+    async submitHandler(e){
+        e.preventDefault();
+        const {username,password} = this.state; 
+        this.props.login(username,password);
+    };
+   
+
+    render() {
+        const { password, username} = this.state;
+        // Redirect if logged in 
+        if(this.props.isAuth){
+            return <Redirect to="/feed"/>
+        }
+
+        return (
+            <div className='bg-gradient'>
             <div className="main__logo">
                 <img src="/images/logo.png" alt="" />
             </div>
             <div className="login-container">
                 <h2>Login</h2>
                 <a href="/"><i class="fas fa-chevron-left"></i></a>
-                <form className="fields">
-                    <input type="text" placeholder=" Username" className="input-fields"/>
-                    <input type="password" placeholder=" Password" className="input-fields"/>
-                    <input type="submit" value="LOGIN" class="login-button" />
+                <Alert/>
+                <form className="fields"  onSubmit={this.submitHandler}>
+                    <input type="text" value={username}   name="username" placeholder=" Username" className="input-fields" onChange={this.changeHandler}/>
+                    <input type="password" value={password} name="password" placeholder=" Password" className="input-fields" onChange={this.changeHandler}/>
+                    <input type="submit"  class="login-button" />
                 </form>
             </div>
             
         </div>
-    )
-}
+        )
+    }
+};
+
+Login.propTypes = {
+    login : PropTypes.func.isRequired,
+    isAuth: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+    isAuth : state.auth.isAuth
+});
+
+export default connect(mapStateToProps,{ login })(Login);
